aba95a8d1ebf60c4d5b9eff86d0977ace09007bc
Martin Dobrev <martindobrev87@gmail.com>
2018-01-04 21:17:09 +0200

    developing the trend-extract feature, eslint rules updated and adding unit tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
-function sayHiTo(name) {
-    return `Hi, ${name}`;
+function extractTrend(inputArr) {
+    const uniques = []
+    let inputArrString
+
+    if (!inputArr) return []
+
+    if (typeof inputArr === 'string') {
+        inputArrString = inputArr.split('')
+    } else if (typeof inputArr === 'number') {
+        inputArrString = inputArr.toString().split('').map(item => (typeof item === 'number' ? item.toString() : item))
+    } else if (typeof inputArr === 'object') {
+        inputArrString = inputArr.map(item => (typeof item === 'number' ? item.toString() : item))
+    }
+
+    const trend = inputArrString.reduce((trendTotal, word) => {
+        const reps = inputArrString.reduce((total, el) => (word === el ? total + 1 : total), 0)
+
+        if (!uniques.includes(word)) {
+            uniques.push(word)
+            trendTotal.push({ word, reps })
+        }
+        return trendTotal
+    }, []).sort((a, b) => b.reps - a.reps)
+
+    return trend
 }
 
-sayHiTo('Bruno');
+export default extractTrend
--- test/index.js
+++ test/index.js
@@ -1,9 +1,53 @@
-import assert from 'assert';
+import * as assert from 'assert'
+import extractTrend from '../src/index'
+
+const testSamples = [
+    {
+        input: ['foo', 'bar', 'zar', 'foo'],
+        expectedResult: [{ word: 'foo', reps: 2 }, { word: 'bar', reps: 1 }, { word: 'zar', reps: 1 }],
+        description: 'should return [{ word: \'foo\', reps: 2 }, { word: \'bar\', reps: 1 }, { word: \'zar\', reps: 1 }] value when receiving [\'foo\', \'bar\', \'zar\', \'foo\']',
+    },
+    {
+        input: ['1', '2', '3', '4', '2', '2', '1'],
+        expectedResult: [{ word: '2', reps: 3 }, { word: '1', reps: 2 }, { word: '3', reps: 1 }, { word: '4', reps: 1 }],
+        description: 'should return [{ word: \'2\', reps: 3 }, { word: \'1\', reps: 2 }, { word: \'3\', reps: 1 }, { word: \'4\', reps: 1 }] value when receiving [\'1\', \'2\', \'3\', \'4\', \'2\', \'2\', \'1\']',
+    },
+    {
+        input: [1, 2, 3, 4, 2, 2, 1],
+        expectedResult: [{ word: '2', reps: 3 }, { word: '1', reps: 2 }, { word: '3', reps: 1 }, { word: '4', reps: 1 }],
+        description: 'should return [{ word: \'2\', reps: 3 }, { word: \'1\', reps: 2 }, { word: \'3\', reps: 1 }, { word: \'4\', reps: 1 }] value when receiving [1, 2, 3, 4, 2, 2, 1]',
+    },
+    {
+        input: [],
+        expectedResult: [],
+        description: 'should return [] value when receiving []',
+    },
+    {
+        input: 'abcadeab',
+        expectedResult: [{ word: 'a', reps: 3 }, { word: 'b', reps: 2 }, { word: 'c', reps: 1 }, { word: 'd', reps: 1 }, { word: 'e', reps: 1 }],
+        description: 'should return [{ word: \'a\', reps: 3 }, { word: \'b\', reps: 2 }, { word: \'c\', reps: 1 }, { word: \'d\', reps: 1 }, { word: \'e\', reps: 1 }] value when receiving abcadeab',
+    },
+    {
+        input: 12313313,
+        expectedResult: [{ word: '3', reps: 4 }, { word: '1', reps: 3 }, { word: '2', reps: 1 }],
+        description: 'should return [{ word: \'3\', reps: 4 }, { word: \'1\', reps: 3 }, { word: \'2\', reps: 1 }] value when receiving 12313313',
+    },
+    {
+        input: '',
+        expectedResult: [],
+        description: 'should return [] value when receiving ',
+    },
+    {
+        input: null,
+        expectedResult: [],
+        description: 'should return [] value when receiving null',
+    },
+]
 
 describe('Array', () => {
-    describe('#indexOf()', () => {
-        it('should return -1 when the value is not present', () => {
-            assert.equal([1, 2, 3].indexOf(4), -1);
-        });
-    });
-});
+    testSamples.forEach((sample) => {
+        it(sample.description, () => {
+            assert.deepEqual(extractTrend(sample.input), sample.expectedResult)
+        })
+    })
+})
